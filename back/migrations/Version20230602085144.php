599a6ca373023751d2fe8d0ea29ea2e05eeca3d8
Quentin RIDEL <q.ridel@attineos.com>
2023-06-02 14:33:06 +0200

    (fix): vulnerability model and added endpoints

---
+++ back/migrations/Version20230602085144.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230602085144 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ip CHANGE perimeter_id perimeter_id CHAR(36) NOT NULL COMMENT \'(DC2Type:uuid)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE ip CHANGE perimeter_id perimeter_id CHAR(36) DEFAULT NULL COMMENT \'(DC2Type:uuid)\'');
+    }
+}
